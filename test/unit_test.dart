34e994dd4029d7bc2e056f71a05e2231ad03ae64
Francisco Gabriel <franciscogabrielf11@gmail.com>
2024-09-06 14:28:07 -0300

    feat: unit tests

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,8 +1,21 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:test_boilerplate/person.dart';
 
 void main() {
-  final person = Person(name: 'Chico', age: 22, height: 1.80, weight: 92);
+  final person = Person(name: 'Chico', age: 16, height: 1.80, weight: 92);
+  setUp(
+    () {
+      print('Inicia o test');
+    },
+  );
+
+  tearDown(
+    () {
+      print('Finaliza o test');
+    },
+  );
 
   test(
     'Imc deve vir 28.4',
@@ -10,4 +23,20 @@ void main() {
       expect(person.imc, 28.4);
     },
   );
+
+  group('Testes de idade', () {
+    test(
+      'Se idade maior ou igual a 18, isOlder deve ser true',
+      () {
+        expect(person.isOlder, true);
+      },
+    );
+
+    test(
+      'Se idade menor que 18, isOlder deve ser false',
+      () {
+        expect(person.isOlder, false);
+      },
+    );
+  });
 }
