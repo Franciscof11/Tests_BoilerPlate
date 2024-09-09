a9fa71040935530b36c561416601ccad446e4e8c
Francisco Gabriel <franciscogabrielf11@gmail.com>
2024-09-09 11:52:31 -0300

    feat: person repository

--- lib/person.dart
+++ lib/person.dart
@@ -6,7 +6,12 @@ class Person {
   final double height;
   final double weight;
 
-  Person({required this.name, required this.age, required this.height, required this.weight});
+  Person({
+    required this.name,
+    required this.age,
+    required this.height,
+    required this.weight,
+  });
 
   double get imc {
     var result = weight / math.pow(height, 2);
@@ -16,5 +21,14 @@ class Person {
 
   bool get isOlder {
     return age >= 18;
+  }
+
+  factory Person.fromMap(Map<String, dynamic> map) {
+    return Person(
+      name: map['name'] ?? '',
+      age: map['age']?.toInt() ?? 0,
+      height: map['height']?.toDouble() ?? 0.0,
+      weight: map['weight']?.toDouble() ?? 0.0,
+    );
   }
 }
---
+++ lib/person_repository.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:test_boilerplate/person.dart';
+
+class PersonRepository {
+  Future<List<Person>> getAllPersons() async {
+    try {
+      final dio = Dio();
+
+      final response = await dio.get('https://66df089fde4426916ee356e5.mockapi.io/person/persons');
+
+      if (response.statusCode == 200) {
+        final responseMap = response.data;
+
+        final List<Person> personList = responseMap.map<Person>((person) => Person.fromMap(person));
+
+        return personList;
+      } else {
+        throw Exception('Erro na requisição: ${response.statusCode} - ${response.statusMessage}');
+      }
+    } catch (e) {
+      throw Exception('Erro: $e');
+    }
+  }
+}
